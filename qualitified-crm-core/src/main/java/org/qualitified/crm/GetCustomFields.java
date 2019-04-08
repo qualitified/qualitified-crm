9a03d7b40dde4c5118c7e0cbd8aad2d3ed0ecff7
Mohamed MAKNI <mmakni@qualitified.com>
2019-04-08 17:00:18 +0100

    Minor fixes

--- qualitified-crm-core/src/main/java/org/qualitified/crm/GetCustomFields.java
+++ qualitified-crm-core/src/main/java/org/qualitified/crm/GetCustomFields.java
@@ -35,7 +35,6 @@ public class GetCustomFields {
 
     @OperationMethod
     public Blob run() throws Exception {
-        logger.error("TEST1234567890");
         LoginContext lc = Framework.loginAsUser("Administrator");
         ctx.getLoginStack().push(lc);
         DocumentModelList customDocuments = ctx.getCoreSession().query("SELECT * FROM Custom WHERE ecm:isProxy = 0 AND ecm:isTrashed = 0 AND ecm:isCheckedInVersion = 0 AND ecm:currentLifeCycleState != 'deleted' AND dc:title='"+documentType+"'", "NXQL", null, 0, 0, false);
