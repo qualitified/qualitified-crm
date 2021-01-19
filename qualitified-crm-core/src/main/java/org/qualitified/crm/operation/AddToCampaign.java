8fea9f390c00ca38169ed41ab22e7f96c1271a8c
Mabrouka JRAD <mabrouka.jrad@gmail.com>
2021-01-19 12:44:09 +0100

    no message

---
+++ qualitified-crm-core/src/main/java/org/qualitified/crm/operation/AddToCampaign.java
@@ -0,0 +1,13 @@
+package org.qualitified.crm.operation;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+
+@Operation(id = AddToCampaign.ID, category = Constants.CAT_EXECUTION, label = "Add To Campaign", description = "...")
+
+
+public class AddToCampaign {
+    public final static String ID = "Qualitified.AddToCampaign";
+
+
+}
