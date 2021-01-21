4dab284d680e2b0a47bc0de8b1b14b22c97fc3d7
BilelFS <fs.bilel@gmail.com>
2021-01-21 18:16:58 +0100

    AddToCampaignBulk implementation

--- qualitified-crm-core/src/main/java/org/qualitified/crm/operation/AddToCampaignBulk.java
+++ qualitified-crm-core/src/main/java/org/qualitified/crm/operation/AddToCampaignBulk.java
@@ -1,154 +1,101 @@
 package org.qualitified.crm.operation;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.operations.services.bulk.AutomationBulkAction;
 import org.nuxeo.ecm.automation.core.util.PageProviderHelper;
 import org.nuxeo.ecm.automation.core.util.Properties;
 import org.nuxeo.ecm.automation.core.util.StringList;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.NuxeoException;
-import org.nuxeo.ecm.core.bulk.BulkAdminService;
 import org.nuxeo.ecm.core.bulk.BulkService;
-import org.nuxeo.ecm.core.bulk.io.BulkParameters;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
 import org.nuxeo.ecm.core.bulk.message.BulkStatus;
-import org.nuxeo.ecm.core.bulk.message.BulkCommand.Builder;
 import org.nuxeo.ecm.platform.query.api.PageProvider;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
+import org.nuxeo.ecm.platform.query.api.PageProviderService;
+import org.nuxeo.runtime.api.Framework;
 
-@Operation(
-        id = AddToCampaignBulk.ID,
-        category = "Services",
-        label = "Run a bulk command",
-        addToStudio = true,
-        description = "Run a bulk action on a set of documents expressed by a NXQL."
-)
-public class AddToCampaignBulk
- {
-    public static final String ID = "Bulk.AddToCampaignBulk";
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Operation(id = AddToCampaignBulk.ID, category = Constants.CAT_EXECUTION, label = "AddToCampaignBulk", description = "EmailingHistoryProcess...")
+public class AddToCampaignBulk {
+
+    public final static String ID = "Qualitified.AddToCampaignBulk";
+    private Log logger = LogFactory.getLog(AddToCampaignBulk.class);
+
+
     @Context
-    protected BulkService service;
-    @Context
-    protected BulkAdminService admin;
+    protected OperationContext ctx;
+
     @Context
     protected CoreSession session;
-    @Param(
-            name = "query",
-            required = false
-    )
+
+    @Param(name = "query", required = false)
     protected String query;
-     @Param(name = "collection")
-     protected DocumentModel collection;
-    @Param(
-            name = "providerName",
-            required = false
-    )
+
+    @Param(name = "providerName", required = false)
     protected String providerName;
-    @Param(
-            name = "queryParams",
-            required = false
-    )
+
+    @Param(name = "queryParams", required = false)
     protected StringList queryParams;
-    @Param(
-            name = "namedParameters",
-            required = false,
-            description = "Named parameters to pass to the page provider to fill in query variables."
-    )
+
+    @Param(name = PageProviderService.NAMED_PARAMETERS, required = false, description = "Named parameters to pass to the page provider to "
+            + "fill in query variables.")
     protected Properties namedParameters;
-    @Param(
-            name = "quickFilters",
-            required = false,
-            description = "Quick filter properties (separated by comma)"
-    )
-    protected StringList quickFilters;
-    @Param(
-            name = "action",
-            required = true
-    )
-    protected String action;
-    @Param(
-            name = "repositoryName",
-            required = false
-    )
-    protected String repositoryName;
-    @Param(
-            name = "bucketSize",
-            required = false
-    )
-    protected int bucketSize;
-    @Param(
-            name = "batchSize",
-            required = false
-    )
-    protected int batchSize;
-    @Param(
-            name = "parameters",
-            required = false
-    )
-    protected String parametersAsJson;
-     protected Map<String, Serializable> params = new HashMap();
+    @Param(name = "collection")
+    protected DocumentModel collection;
 
+    Map<String, Serializable> Params =new HashMap<>();
+    @OperationMethod
+    public void run() throws OperationException, LoginException, JSONException, InterruptedException {
+        LoginContext lc = Framework.loginAsUser("Administrator");
+        ctx.getLoginStack().push(lc);
+        Params.put("collection",collection);
 
-     public AddToCampaignBulk() {
-    }
-
-    @OperationMethod(
-            asyncService = BulkService.class
-    )
-    public BulkStatus run() throws IOException, OperationException {
-        if (!this.admin.getActions().contains(this.action)) {
-            throw new NuxeoException("Action '" + this.action + "' not found", 404);
-        } else if (!this.admin.isHttpEnabled(this.action) && !this.session.getPrincipal().isAdministrator()) {
-            throw new NuxeoException("Action '" + this.action + "' denied", 403);
-        } else if (this.query == null && this.providerName == null) {
+        if (query == null && providerName == null) {
             throw new OperationException("Query and ProviderName cannot be both null");
-        } else {
-            String userName = this.session.getPrincipal().getName();
-            PageProviderDefinition def = this.query != null ? PageProviderHelper.getQueryPageProviderDefinition(this.query) : PageProviderHelper.getPageProviderDefinition(this.providerName);
-            if (def == null) {
-                throw new OperationException("Could not get Provider Definition from either query or provider name");
-            } else {
-                PageProvider<?> provider = PageProviderHelper.getPageProvider(this.session, def, this.namedParameters, this.queryParams != null ? this.queryParams.toArray(new String[0]) : null);
-                this.query = PageProviderHelper.buildQueryStringWithAggregates(provider);
-                if (this.query.contains("?")) {
-                    throw new OperationException("Query parameters could not be parsed");
-                } else {
-                    params.put("collection",collection);
-                    Builder builder = (new Builder(this.action, this.query)).user(userName).params(this.params);
-
-                    try {
-                        builder.params(BulkParameters.paramsToMap(this.parametersAsJson));
-                    } catch (IOException var6) {
-                        throw new OperationException("Could not parse parameters, expecting valid json value", var6);
-                    }
-
-                    if (this.repositoryName != null) {
-                        builder.repository(this.repositoryName);
-                    } else {
-                        builder.repository(this.session.getRepositoryName());
-                    }
-
-                    if (this.bucketSize > 0) {
-                        builder.bucket(this.bucketSize);
-                    }
-
-                    if (this.batchSize > 0) {
-                        builder.batch(this.batchSize);
-                    }
-
-                    String commandId = this.service.submit(builder.build());
-                    return (BulkStatus)this.service.getStatus(commandId);
-                }
-            }
         }
+        PageProviderDefinition def = query != null ? PageProviderHelper.getQueryPageProviderDefinition(query)
+                : PageProviderHelper.getPageProviderDefinition(providerName);
+
+        if (def == null) {
+            throw new OperationException("Could not get Provider Definition from either query or provider name");
+        }
+
+        PageProvider<?> provider = PageProviderHelper.getPageProvider(session, def, namedParameters,
+                queryParams != null ? queryParams.toArray(new String[0]) : null);
+        query = PageProviderHelper.buildQueryStringWithAggregates(provider);
+
+        BulkCommand fetchMailHistoryCommand = new BulkCommand.Builder(AutomationBulkAction.ACTION_NAME, query)
+                .repository("default")
+                .user("Administrator")
+                .param(AutomationBulkAction.OPERATION_ID, "Qualitified.AddToCampaign")
+                .param(AutomationBulkAction.OPERATION_PARAMETERS, (Serializable) Params)
+                .build();
+
+        // run command
+        BulkService BulkService = Framework.getService(BulkService.class);
+        String CommandId = BulkService.submit(fetchMailHistoryCommand);
+
+        // await end of computation
+        //BulkService.await(CommandId, Duration.ofMinutes(1));
+
+        // get status
+        BulkStatus BulkStatus = BulkService.getStatus(CommandId);
+
     }
 }
-
