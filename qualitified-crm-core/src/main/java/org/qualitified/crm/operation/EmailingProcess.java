7bd9116dff647a320cf04eb2841de23a44868eed
BilelFS <fs.bilel@gmail.com>
2021-02-19 14:50:23 +0100

    Handling Automated Emailing including all the steps

--- qualitified-crm-core/src/main/java/org/qualitified/crm/operation/EmailingProcess.java
+++ qualitified-crm-core/src/main/java/org/qualitified/crm/operation/EmailingProcess.java
@@ -2,6 +2,7 @@ package org.qualitified.crm.operation;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.h2.util.StringUtils;
 import org.json.JSONException;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
@@ -10,7 +11,6 @@ import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.operations.services.bulk.AutomationBulkAction;
-import org.nuxeo.ecm.automation.core.scripting.DocumentWrapper;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
@@ -43,6 +43,9 @@ public class EmailingProcess {
     protected CoreSession documentManager;
 
     Map<String, Serializable> sendMailBulkParams =new HashMap<>();
+    String contactsQuery;
+
+
 
     @OperationMethod
     public void run() throws OperationException, LoginException, JSONException, InterruptedException, ParseException {
@@ -57,7 +60,7 @@ public class EmailingProcess {
                         "AND ecm:isProxy = 0 AND ecm:isCheckedInVersion = 0  " +
                         "AND ecm:currentLifeCycleState != 'deleted' " +
                         "AND ecm:isTrashed = 0");
-        
+
         if ( !campaignDocuments.isEmpty() ) {
             for (DocumentModel campaignDoc : campaignDocuments) {
                 triggerSendMailBulk(campaignDoc);
@@ -77,32 +80,27 @@ public class EmailingProcess {
         } else logger.error("This campaign is not ready yet to be sent, please check its status");
 
     }
-    private BulkStatus sendMailBulk(Map<String, Serializable> sendMailBulkParams) throws InterruptedException {
+    private BulkStatus sendMailBulk(Map<String, Serializable> sendMailBulkParams, String contactsQuery) throws InterruptedException {
+        if (contactsQuery!= null) {
+            BulkCommand prepareMailCommand = new BulkCommand.Builder(AutomationBulkAction.ACTION_NAME, contactsQuery)
+                    .repository("default")
+                    .user("Administrator")
+                    .param(AutomationBulkAction.OPERATION_ID,"Qualitified.SendMail")
+                    .param(AutomationBulkAction.OPERATION_PARAMETERS, (Serializable) sendMailBulkParams)
+                    .build();
 
-        BulkCommand prepareMailCommand = new BulkCommand.Builder(AutomationBulkAction.ACTION_NAME,
-                "SELECT * FROM Document " +
-                        "WHERE ecm:primaryType IN ('Contact','Silhouette')" +
-                        "AND ecm:mixinType != 'HiddenInNavigation'" +
-                        "AND ecm:isCheckedInVersion = 0 " +
-                        "AND ecm:currentLifeCycleState !='deleted'" +
-                        "AND ecm:isTrashed = 0 " +
-                        "AND collectionMember:collectionIds/* = '"+sendMailBulkParams.get("campaignId")+"' ")
-                .repository("default")
-                .user("Administrator")
-                .param(AutomationBulkAction.OPERATION_ID,"Qualitified.SendMail")
-                .param(AutomationBulkAction.OPERATION_PARAMETERS, (Serializable) sendMailBulkParams)
-                .build();
+            // run command
+            BulkService prepareMailBulkService = Framework.getService(BulkService.class);
+            String prepareMailCommandId = prepareMailBulkService.submit(prepareMailCommand);
 
-        // run command
-        BulkService prepareMailBulkService = Framework.getService(BulkService.class);
-        String prepareMailCommandId = prepareMailBulkService.submit(prepareMailCommand);
+            // await end of computation
+            //prepareMailBulkService.await(prepareMailCommandId, Duration.ofMinutes(1));
 
-        // await end of computation
-        //prepareMailBulkService.await(prepareMailCommandId, Duration.ofMinutes(1));
-
-        // get status
-        BulkStatus prepareMailBulkStatus = prepareMailBulkService.getStatus(prepareMailCommandId);
-        return prepareMailBulkStatus;
+            // get status
+            BulkStatus prepareMailBulkStatus = prepareMailBulkService.getStatus(prepareMailCommandId);
+            return prepareMailBulkStatus;
+        }
+        return null;
     }
     private void triggerSendMailBulk(DocumentModel campaignDoc) throws InterruptedException, ParseException {
 
@@ -110,11 +108,12 @@ public class EmailingProcess {
         documentManager.saveDocument(campaignDoc);
         IdRef automationDocId = new IdRef((String) campaignDoc.getPropertyValue("campaign:automationId"));
         DocumentModel automationDoc = documentManager.getDocument(automationDocId);
-
         IdRef emailDocId;
+        DocumentModel emailDoc;
         String emailStepZero = (String) automationDoc.getPropertyValue("custom:documentField1");
         String emailStepOne = (String) automationDoc.getPropertyValue("custom:documentField2");
         String emailStepTwo = (String) automationDoc.getPropertyValue("custom:documentField3");
+        String emailStepThree = (String) automationDoc.getPropertyValue("custom:documentField4");
 
         int waitStepOneAmount = automationDoc.getPropertyValue("custom:integerField1")!= null
                 ? Math.toIntExact((long) automationDoc.getPropertyValue("custom:integerField1"))
@@ -122,29 +121,39 @@ public class EmailingProcess {
         int waitStepTwoAmount = automationDoc.getPropertyValue("custom:integerField2")!= null
                 ? Math.toIntExact((long) automationDoc.getPropertyValue("custom:integerField2"))
                 : 0 ;
+        int waitStepThreeAmount = automationDoc.getPropertyValue("custom:integerField3")!= null
+                ? Math.toIntExact((long) automationDoc.getPropertyValue("custom:integerField3"))
+                : 0 ;
         int currentStep = campaignDoc.getPropertyValue("custom:integerField10")!= null
                 ? Math.toIntExact((long) campaignDoc.getPropertyValue("custom:integerField10"))
                 : 0 ;
-
         GregorianCalendar currentSendDate = (GregorianCalendar) campaignDoc.getPropertyValue("campaign:sendDate");
-
         switch (currentStep) {
+            case 3:
+                emailDocId = new IdRef(emailStepThree);
+                setContactsQuery(campaignDoc.getId(), emailStepTwo, true);
+                stepUpdate(campaignDoc,null,0,currentStep,currentSendDate,3);
+                break;
+
             case 2:
                 emailDocId = new IdRef(emailStepTwo);
-                stepUpdate(campaignDoc,null,0,currentStep,null,3);
+                setContactsQuery(campaignDoc.getId(), emailStepOne, true);
+                stepUpdate(campaignDoc, emailStepThree,waitStepThreeAmount,currentStep,currentSendDate,3);
                 break;
 
             case 1:
                 emailDocId = new IdRef(emailStepOne);
-                stepUpdate(campaignDoc,emailStepTwo,waitStepTwoAmount,currentStep,currentSendDate,2);
+                setContactsQuery(campaignDoc.getId(), emailStepZero, false);
+                stepUpdate(campaignDoc, emailStepTwo,waitStepTwoAmount,currentStep,currentSendDate,2);
                 break;
 
             default :
                 emailDocId = new IdRef(emailStepZero);
-                stepUpdate(campaignDoc,emailStepOne,waitStepOneAmount,currentStep,currentSendDate,1);
+                setContactsQuery(campaignDoc.getId(), null, false);
+                stepUpdate(campaignDoc, emailStepOne,waitStepOneAmount,currentStep,currentSendDate,1);
         }
 
-        DocumentModel emailDoc = documentManager.getDocument(emailDocId);
+        emailDoc = documentManager.getDocument(emailDocId);
 
         sendMailBulkParams.put("campaignId", campaignDoc.getId());
         sendMailBulkParams.put("emailId", emailDoc.getId());
@@ -152,29 +161,78 @@ public class EmailingProcess {
         sendMailBulkParams.put("htmlPart", emailDoc.getPropertyValue("html:content"));
 
         // run operation that will trigger the send MailBulk
-        sendMailBulk(sendMailBulkParams);
+        sendMailBulk(sendMailBulkParams, contactsQuery);
     }
 
 
     private void stepUpdate(DocumentModel campaignDoc, String emailNextStep, int waitNextStepAmount, int currentStep, GregorianCalendar currentSendDate, int nextStep) throws InterruptedException, ParseException {
         if ( emailNextStep != null ) {
-            GregorianCalendar nextSendDate = addDaysCalendar(currentSendDate, waitNextStepAmount);
             campaignDoc.setPropertyValue("custom:integerField10",nextStep);
-            campaignDoc.setPropertyValue("custom:stringField1","Step"+nextStep);
-            campaignDoc.setPropertyValue("campaign:sendDate", nextSendDate);
             campaignDoc.setPropertyValue("campaign:status", "Ready");
 
         } else {
-           /* campaignDoc.setPropertyValue("custom:integerField10",currentStep);
-            campaignDoc.setPropertyValue("custom:stringField1","Step"+currentStep);*/
             campaignDoc.setPropertyValue("campaign:status", "Sent");
         }
+        Date nextSendDate = addDaysCalendar(currentSendDate, waitNextStepAmount);
+        campaignDoc.setPropertyValue("campaign:sendDate", nextSendDate);
+        campaignDoc.setPropertyValue("custom:documentField9",emailNextStep);
         documentManager.saveDocument(campaignDoc);
     }
 
-    private static GregorianCalendar addDaysCalendar(GregorianCalendar date, int amount) throws ParseException {
+    private static Date addDaysCalendar(GregorianCalendar date, int amount) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        date.add(GregorianCalendar.DATE,amount);
-        return date;
+        GregorianCalendar newDate = new GregorianCalendar();
+        newDate.setTime(date.getTime());
+        newDate.add(GregorianCalendar.DATE,amount);
+        return newDate.getTime();
+    }
+
+    private String setContactsQuery(String campaignId, String emailId, Boolean doFilter) {
+        String activeContacts;
+        if ( doFilter == true ){
+            DocumentModelList interactionDocuments = documentManager
+                    .query("SELECT * FROM Interaction WHERE interaction:campaignId= '"+ campaignId+ "' " +
+                            "AND custom:documentField10= '"+ emailId+ "' " +
+                            "AND interaction:isOpened = 1 " +
+                            "AND interaction:activity= 'Emailing' " +
+                            "AND interaction:status= 'DONE' " +
+                            "AND interaction:messageID IS NOT NULL " +
+                            "AND ecm:isProxy = 0 AND ecm:isTrashed = 0 AND ecm:isCheckedInVersion = 0 " +
+                            "AND ecm:currentLifeCycleState != 'deleted' ");
+            ArrayList<String> personList = new ArrayList<>();
+
+            if ( !interactionDocuments.isEmpty() ) {
+                for (DocumentModel interactionDoc : interactionDocuments) {
+                    String[] person = (String[]) interactionDoc.getPropertyValue("interaction:contact");
+                    String formattedPerson ="'"+person[0]+"'";
+                    personList.add(formattedPerson);
+                }
+                activeContacts = personList.toString().replace("[","(").replace("]",")");
+                contactsQuery = "SELECT * FROM Document " +
+                        "WHERE ecm:primaryType IN ('Contact','Silhouette')" +
+                        "AND ecm:uuid IN " + activeContacts + " " +
+                        "AND custom:integerField1 = 0 " +
+                        "AND ecm:mixinType != 'HiddenInNavigation'" +
+                        "AND ecm:isCheckedInVersion = 0 " +
+                        "AND ecm:currentLifeCycleState !='deleted'" +
+                        "AND ecm:isTrashed = 0 " +
+                        "AND collectionMember:collectionIds/* = '" + campaignId + "' ";
+            } else {
+                contactsQuery = null;
+            }
+            return contactsQuery;
+
+        } else {
+            contactsQuery = "SELECT * FROM Document " +
+                    "WHERE ecm:primaryType IN ('Contact','Silhouette')" +
+                    "AND (custom:integerField1 IS NULL OR custom:integerField1 = 0) " +
+                    "AND ecm:mixinType != 'HiddenInNavigation'" +
+                    "AND ecm:isCheckedInVersion = 0 " +
+                    "AND ecm:currentLifeCycleState !='deleted'" +
+                    "AND ecm:isTrashed = 0 " +
+                    "AND collectionMember:collectionIds/* = '" + campaignId + "' ";
+        }
+
+        return contactsQuery;
     }
 }
