5d0cc65ead1ea6c57a26e51e8786d8dd71c6137a
fatnassibilel <fs.bilel@gmail.com>
2024-08-30 20:00:45 +0200

    modifs migration lts2023

--- qualitified-crm-core/src/test/java/org/qualitified/crm/TestStudioBundle.java
+++ qualitified-crm-core/src/test/java/org/qualitified/crm/TestStudioBundle.java
@@ -3,23 +3,21 @@ package org.qualitified.crm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.automation.AutomationService;
-import org.nuxeo.ecm.automation.OperationContext;
-import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
-import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.test.runner.*;
 import org.nuxeo.runtime.RuntimeService;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
+
 import javax.inject.Inject;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import org.nuxeo.ecm.automation.test.AutomationFeature;
-import org.nuxeo.ecm.directory.sql.SQLDirectoryFeature;
-import org.nuxeo.ecm.core.api.DocumentModel;
 
 @RunWith(FeaturesRunner.class)
 @Features({CoreFeature.class, RuntimeFeature.class, AutomationFeature.class})
